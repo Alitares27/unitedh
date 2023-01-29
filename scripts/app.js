976fdc3ebdb03b6e7b33c4d01bacfe1f15d74850
AldairPay <pay22002@byui.edu>
2023-01-28 23:19:41 -0500

    update

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,40 @@ document.getElementById("modifiedDate").innerHTML = fullDate;
 let currentDate = new Date()
 let currentYear = currentDate.getFullYear()
 document.getElementById('year').innerHTML = currentYear;
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const newdate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById('date-box').innerHTML = newdate
+
+function togglemenu(){
+document.getElementById("primaryNav").classList.toggle("open");
+document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn")
+x.onclick = togglemenu;
